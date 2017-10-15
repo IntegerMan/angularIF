ef609d377e34aa9a353a8d2d2b43b19cf998e779
Matt Eland <matt.eland@gmail.com>
2017-10-15 00:45:25 -0400

    Added verb module.

--- src/app/engine/engine.module.ts
+++ src/app/engine/engine.module.ts
@@ -5,11 +5,13 @@ import {CommandParserService} from './tokenizer/command-parser.service';
 import {TokenizerModule} from './tokenizer/tokenizer.module';
 import {TextOutputService} from './text-output.service';
 import {UserInputService} from './user-input.service';
+import {VerbsModule} from './verbs/verbs.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    TokenizerModule
+    TokenizerModule,
+    VerbsModule
   ],
   providers: [
     InteractiveFictionService,
