c91a8810782a4a6bb867ffefc60d2baeecc5d4ea
Matt Eland <matt.eland@gmail.com>
2017-10-18 00:09:52 -0400

    angularIF-37 Give each WorldEntity a size and weight
    Added enums for size / weight.

---
+++ src/app/engine/entity-weight.enum.ts
@@ -0,0 +1,16 @@
+/**
+ * An enum containing a list of comparative weights for entities named after easy to consider objects
+ */
+export enum EntityWeight {
+  feather = 0,
+  pocketchange = 1,
+  watch = 2,
+  cellphone = 3,
+  textbook = 4,
+  backpack = 5,
+  person = 6,
+  minivan = 7,
+  citybus = 8,
+  building = 9,
+  cityblock = 10
+}
