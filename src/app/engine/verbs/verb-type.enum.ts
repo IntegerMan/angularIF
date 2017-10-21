4d4a390517617fcf238d47d772e80abe747a3188
Matt Eland <matt.eland@gmail.com>
2017-10-21 01:49:58 -0400

    Wat

---
+++ src/app/engine/verbs/verb-type.enum.ts
@@ -0,0 +1,9 @@
+export enum VerbType {
+
+  system = 0,
+  look = 1,
+  manipulate = 2,
+  social = 3,
+  go = 4
+
+}
---
+++ src/app/stories/cloak-of-darkness/cloak.ts
@@ -0,0 +1,38 @@
+import {CommandContext} from '../../engine/command-context';
+import {Room} from '../../engine/entities/room';
+import {PortableEntity} from '../../engine/entities/portable-entity';
+import {EntitySize} from '../../engine/entities/entity-size.enum';
+import {EntityWeight} from '../../engine/entities/entity-weight.enum';
+
+export class Cloak extends PortableEntity {
+
+  cloakroom: Room;
+
+  constructor() {
+    super('black velvet cloak');
+
+    this.weight = EntityWeight.textbook;
+    this.size = EntitySize.person;
+
+    this.description = 'A handsome cloak, of velvet trimmed with satin, and slightly spattered with raindrops. ' +
+      'Its blackness is so deep that it almost seems to suck light from the room.';
+
+    this.examineDescription = 'The velvet cloak almost seems to have darkness woven into its very fabric. ' +
+      'Everything about it is darker than it should be.';
+
+    this.inRoomDescription = 'A dark black velvet cloak rests in a heap on the floor.';
+
+    this.addAdjectiveAlias('smart');
+  }
+
+  allowDrop(context: CommandContext): boolean {
+
+    // Don't allow dropping the cloak anywhere but the cloakroom
+    if (context.currentRoom !== this.cloakroom) {
+      context.outputService.displayFailedAction('This isn\'t the best place to leave a smart cloak lying around.');
+      return false;
+    }
+
+    return super.allowDrop(context);
+  }
+}
