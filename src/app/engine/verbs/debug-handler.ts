e1629510dd8faee4397c8b4ac51f161b660874ff
Matt Eland <matt.eland@gmail.com>
2017-10-24 01:33:22 -0400

    #65 - Debug Verb
    Debug is now functional. I want a better tree-style display and more properties, but we're online for now.

--- src/app/engine/verbs/debug-handler.ts
+++ src/app/engine/verbs/debug-handler.ts
@@ -17,21 +17,26 @@ export class DebugHandler extends VerbHandler {
 
   handleCommand(command: Command, context: CommandContext): CommandResult {
 
-    if (environment.production) {
-      context.outputService.displaySystem('Debugging aids are not available in production.');
+    if (environment.showDebugAids) {
+      context.outputService.displaySystem('Debugging aids are not available.');
       return CommandResult.BuildFreeActionResult();
     }
 
-    if (command.isTargetingAll || command.tokens.filter(t => !t.isInferred).length <= 1) {
+    if (command.tokens.filter(t => !t.isInferred).length <= 1) {
 
+      // Just went with 'debug' interpret it at the room level
       context.outputService.displayEntityDebugInfo(context.currentRoom.name, context.currentRoom);
 
+    } else if (command.isTargetingAll) {
+
+      // List everything inside of the room, including the player.
       for (const obj of context.currentRoom.contents) {
         context.outputService.displayEntityDebugInfo(obj.that, obj);
       }
 
     } else {
 
+      // We have a specific debug string. List the token and entity mapping for each token.
       for (const token of command.tokens) {
 
         if (token.name === 'debug' || token.isInferred) {
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,5 +5,6 @@
 
 export const environment = {
   production: false,
+  showDebugAids: true,
   environmentName: 'localhost'
 };
