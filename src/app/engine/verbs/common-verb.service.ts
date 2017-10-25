7ac5810a8aa2b2af8e63fe23fc1d6703ef586b5c
Matt Eland <matt.eland@gmail.com>
2017-10-24 23:39:48 -0400

    #49 Wait Verb

--- src/app/engine/verbs/common-verb.service.ts
+++ src/app/engine/verbs/common-verb.service.ts
@@ -16,6 +16,7 @@ import {QuitHandler} from './quit-handler';
 import {HangHandler} from './hang-handler';
 import {PutHandler} from './put-handler';
 import {DebugHandler} from './debug-handler';
+import {WaitHandler} from './wait-handler';
 
 @Injectable()
 export class CommonVerbService {
@@ -36,6 +37,7 @@ export class CommonVerbService {
     verbs.push(new DropHandler(['drop', 'discard']));
     verbs.push(new OpenHandler(['open']));
     verbs.push(new DebugHandler(['debug']));
+    verbs.push(new WaitHandler(['wait', 'delay', 'lurk', 'loiter', 'watch']));
     verbs.push(new CloseHandler(['close', 'shut']));
     verbs.push(new RestartHandler(['restart', 'reset']));
     verbs.push(new DieHandler(['die', 'forfeit', 'suicide']));
---
+++ src/app/engine/verbs/wait-handler.ts
@@ -0,0 +1,22 @@
+import {VerbHandler} from './verb-handler';
+import {Command} from '../parser/command';
+import {CommandContext} from '../command-context';
+import {VerbType} from './verb-type.enum';
+import {CommandResult} from '../command-result';
+
+export class WaitHandler extends VerbHandler {
+
+  get verbType(): VerbType {
+    return VerbType.look;
+  }
+
+  handleCommand(command: Command, context: CommandContext): CommandResult {
+
+    context.outputService.displayStory('Time passes...');
+
+    return CommandResult.BuildActionSuccessResult();
+
+  }
+
+
+}
