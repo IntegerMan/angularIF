1f2f3f1ee2feccb1266ee49432dc05adee4d1ce8
Matt Eland <matt.eland@gmail.com>
2017-11-22 01:41:00 -0500

    Added Shebang

--- updateCoverage.sh
+++ updateCoverage.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 CODECLIMATE_API_HOST=https://codebeat.co/webhooks/code_coverage \
 CODECLIMATE_REPO_TOKEN=7c2419dd-3d3f-484e-afb1-773ed152c7a0 \
 codeclimate-test-reporter < coverage/lcov.info
