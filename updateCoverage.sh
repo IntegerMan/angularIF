244a42e73c7528507a63f1aed0cbdd688c5dca61
Matt Eland <matt.eland@gmail.com>
2017-11-22 01:00:40 -0500

    Corrected reference.

--- updateCoverage.sh
+++ updateCoverage.sh
@@ -1,3 +1,3 @@
 CODECLIMATE_API_HOST=https://codebeat.co/webhooks/code_coverage \
 CODECLIMATE_REPO_TOKEN=7c2419dd-3d3f-484e-afb1-773ed152c7a0 \
-codeclimate-test-reporter < coverage/lcov/lcov.info
+codeclimate-test-reporter < coverage/lcov.info
