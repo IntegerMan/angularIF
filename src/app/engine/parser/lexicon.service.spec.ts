bf005bb83630772b83eaa6fa3e1c2ebe4f24bfb8
Matt Eland <matt.eland@gmail.com>
2017-12-11 16:16:42 -0500

    Fixed failing tests.

--- src/app/engine/parser/lexicon.service.spec.ts
+++ src/app/engine/parser/lexicon.service.spec.ts
@@ -1,51 +1,40 @@
-import { TestBed, inject } from '@angular/core/testing';
-
 import { LexiconService } from './lexicon.service';
-import {LoggingService} from '../../utility/logging.service';
 import {NaturalLanguageService} from './natural-language.service';
-import {EngineModule} from '../engine.module';
 import {CommandToken} from './command-token';
+import {CommonDictionary} from './common-dictionary';
 
 describe('LexiconService', () => {
 
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      imports: [ EngineModule ],
-      providers: [ NaturalLanguageService, LexiconService, LoggingService]
-    });
 
+    // Ensure common dictionary terms are added
+    const dict = new CommonDictionary();
+    dict.addTerms();
 
   });
 
-  it('should be created', inject([LexiconService], (service: LexiconService) => {
-    expect(service).toBeTruthy();
-  }));
+  it('should be created',  () => {
+    expect(LexiconService.instance).toBeTruthy();
+  });
 
-  it('should have replacement rules', inject([LexiconService], (service: LexiconService) => {
-    service.useDefaults();
-    expect(service.replacementRuleCount).toBeGreaterThan(0);
-  }));
+  it('should have replacement rules',  () => {
+    expect(LexiconService.instance.replacementRuleCount).toBeGreaterThan(0);
+  });
 
-  it('should have expansion rules', inject([LexiconService], (service: LexiconService) => {
-    service.useDefaults();
-    expect(service.expansionRuleCount).toBeGreaterThan(0);
-  }));
+  it('should have expansion rules',  () => {
+    expect(LexiconService.instance.expansionRuleCount).toBeGreaterThan(0);
+  });
 
-  it('ne evaluates to northeast',
-    inject([LexiconService, NaturalLanguageService],
-      (service: LexiconService, tokenizer: NaturalLanguageService) => {
-    service.useDefaults();
-    const token: CommandToken = tokenizer.processor.getTokenForWord('ne');
-    expect(service.replaceTokens([token], tokenizer.processor)[0].name).toBe('northeast');
-  }));
+  it('ne evaluates to northeast',  () => {
+    const token: CommandToken = NaturalLanguageService.instance.processor.getTokenForWord('ne');
+    expect(LexiconService.instance.replaceTokens([token], NaturalLanguageService.instance.processor)[0].name).toBe('northeast');
+  });
 
-  it('cannot evaluates to can not', inject([LexiconService], (service: LexiconService) => {
-    service.useDefaults();
-    expect(service.replaceWords('cannot')).toBe('can not');
-  }));
+  it('cannot evaluates to can not',  () => {
+    expect(LexiconService.instance.replaceWords('cannot')).toBe('can not');
+  });
 
-  it('north east evaluates to northeast', inject([LexiconService], (service: LexiconService) => {
-    service.useDefaults();
-    expect(service.replaceWords('north east')).toBe('northeast');
-  }));
+  it('north east evaluates to northeast',  () => {
+    expect(LexiconService.instance.replaceWords('north east')).toBe('northeast');
+  });
 });
