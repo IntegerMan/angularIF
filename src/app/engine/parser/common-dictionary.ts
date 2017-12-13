6b6ab5aca264af1c1671e42bd606f1ca25ba34a9
Matt Eland <matt.eland@gmail.com>
2017-12-13 00:57:16 -0500

    Switching from YAML to JSON completely.

--- src/app/engine/parser/common-dictionary.ts
+++ src/app/engine/parser/common-dictionary.ts
@@ -5,7 +5,7 @@ export class CommonDictionary extends LexiconDictionary {
 
   public addTerms(): void {
 
-    const data = require('json-loader!yaml-loader!App/Content/CommonDict.yml');
+    const data = require('json-loader!App/Content/CommonDict.json');
     DictionaryReader.readDictionary(data);
 
   }
