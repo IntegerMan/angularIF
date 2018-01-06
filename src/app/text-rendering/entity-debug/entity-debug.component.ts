cf540a2708b7ea2656b3c433681adf9eab5dda80
Matt Eland <matt.eland@gmail.com>
2018-01-05 22:59:10 -0500

    Debugger display now includes entity refs for contents.

--- src/app/text-rendering/entity-debug/entity-debug.component.ts
+++ src/app/text-rendering/entity-debug/entity-debug.component.ts
@@ -11,54 +11,10 @@ export class EntityDebugComponent implements OnInit {
   @Input()
   entity: WorldEntity;
 
-  contentNodes: any[];
-
   constructor() {
-    this.contentNodes = [];
   }
 
-  ngOnInit() {
-
-    for (const child of this.entity.contents) {
-      this.buildNodeForEntity(child, null);
-    }
-
+  ngOnInit(): void {
   }
 
-  private buildNodeForEntity(entity: WorldEntity, parentNode: any): void {
-
-    // Build the base node
-    const node: any = {
-      label: entity.that,
-      data: entity,
-      parent: parentNode,
-      children: [],
-      draggable: false,
-      droppable: false,
-      selectable: false,
-      expanded: true,
-      leaf: entity.contents.length <= 0
-    };
-
-    // Set to an appropriate document
-    if (node.leaf) {
-      node.icon = 'fa-file-text-o';
-    } else {
-      node.expandedIcon = 'fa-folder-open';
-      node.collapsedIcon = 'fa-folder';
-    }
-
-    // Go recursive to build out child nodes
-    for (const child of entity.contents) {
-      this.buildNodeForEntity(child, node);
-    }
-
-    // Add it to its parent
-    if (parentNode && parentNode !== null) {
-      parentNode.children.push(node);
-    } else {
-      this.contentNodes.push(node);
-    }
-
-  }
 }
